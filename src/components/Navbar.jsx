8d012e070f457687739eec16363cd5fccc271978
Devansh sharma <devanshpandit2004@gmail.com>
2025-11-23 15:27:28 +0530

    little fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,11 @@ import { Link } from "react-router-dom";
 const MyNavbar = () => {
     const cartProducts = useSelector(state => state.cart)
   return (
-      <Navbar className="flex flex-wrap items-end justify-evenly">
+      <Navbar className="flex flex-wrap items-end justify-between">
       <NavbarBrand as={Link} to="/">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-black">Flowbite React</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-black">Products</span>
       </NavbarBrand>
-            <NavbarLink href="/">
-        Product
-    </NavbarLink>
+           
         <NavbarLink as={Link} to="/cart">
         My bag {cartProducts.length}
     </NavbarLink>
